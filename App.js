82a0779f8e7cb1c2842a36f95add625e80d83664
Mohamed Allam <m.allam@sewedy.com.eg>
2023-06-11 16:52:11 +0300

    lesson 19 flexBox

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
 import React from 'react';
 
-import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import {
+  Button,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.textview}>Hello World!!!!</Text>
-      <Text style={styles.textview}>Hello World!!!!</Text>
-      <Text style={styles.textview}>Hello World!!!!</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputCountainer}>
+        <TextInput style={styles.inputText} placeholder="your course goal" />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text> List Of Goals </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  appContainer: {
+    padding: 10,
+  },
+  inputCountainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  inputText: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '80%',
+    marginRight: 8,
+    padding: 8,
+  },
+  flexBox: {
+    flex: 1, // means take all space of screen like weight in android
+    flexDirection: 'column', // flexDirection like oraination in android, column = vertival , row = horizontail
+    justifyContent: 'space-between', //distribute boxs inside layout
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
